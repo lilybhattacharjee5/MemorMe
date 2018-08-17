e0526d876b47d6270e920b65a7e7b2a9654fce94
Lily Bhattacharjee <lilybhattacharjeeca@gmail.com>
2018-08-16 17:22:13 -0700

    Initial Commit

---
+++ Post-it/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Post-it
+//
+//  Created by Lily Bhattacharjee on 8/16/18.
+//  Copyright © 2018 Lily Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
